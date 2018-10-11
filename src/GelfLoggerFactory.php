60269f5dd7f1133b82ed7adab46d17e08726bebd
hedii <contact@hedichaibi.com>
2018-10-11 13:35:46 +0200

    Do not use ParsesLogConfiguration trait (not available in Laravel 5.6)
    Close #5

--- src/GelfLoggerFactory.php
+++ src/GelfLoggerFactory.php
@@ -5,14 +5,28 @@ namespace Hedii\LaravelGelfLogger;
 use Gelf\Publisher;
 use Gelf\Transport\IgnoreErrorTransportWrapper;
 use Gelf\Transport\UdpTransport;
-use Illuminate\Log\ParsesLogConfiguration;
+use InvalidArgumentException;
 use Monolog\Formatter\GelfMessageFormatter;
 use Monolog\Handler\GelfHandler;
 use Monolog\Logger;
 
 class GelfLoggerFactory
 {
-    use ParsesLogConfiguration;
+    /**
+     * The Log levels.
+     *
+     * @var array
+     */
+    protected $levels = [
+        'debug' => Logger::DEBUG,
+        'info' => Logger::INFO,
+        'notice' => Logger::NOTICE,
+        'warning' => Logger::WARNING,
+        'error' => Logger::ERROR,
+        'critical' => Logger::CRITICAL,
+        'alert' => Logger::ALERT,
+        'emergency' => Logger::EMERGENCY,
+    ];
 
     /**
      * Create a custom Monolog instance.
@@ -34,6 +48,39 @@ class GelfLoggerFactory
         $handler->setFormatter(new GelfMessageFormatter(null, null, null));
 
         return new Logger($this->parseChannel($config), [$handler]);
+    }
+
+    /**
+     * Parse the string level into a Monolog constant.
+     *
+     * @param array $config
+     * @return int
+     * @throws \InvalidArgumentException
+     */
+    protected function level(array $config): int
+    {
+        $level = $config['level'] ?? 'debug';
+
+        if (isset($this->levels[$level])) {
+            return $this->levels[$level];
+        }
+
+        throw new InvalidArgumentException('Invalid log level.');
+    }
+
+    /**
+     * Extract the log channel from the given configuration.
+     *
+     * @param array $config
+     * @return string
+     */
+    protected function parseChannel(array $config): string
+    {
+        if (! isset($config['name'])) {
+            return $this->getFallbackChannelName();
+        }
+
+        return $config['name'];
     }
 
     /**
