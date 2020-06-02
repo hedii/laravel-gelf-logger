d492c427044cd398745e112e104adce0a20c8779
rytis <rdereskevicius@gmail.com>
2020-06-02 13:57:28 +0300

    Remove unused local variables

--- src/GelfLoggerFactory.php
+++ src/GelfLoggerFactory.php
@@ -68,10 +68,10 @@ class GelfLoggerFactory
 
         $handler->setFormatter(
             new GelfMessageFormatter(
-                $systemName = $config['system_name'] ?? null,
-                $extraPrefix = null,
-                $contextPrefix = '',
-                $maxLength = $config['max_length'] ?? null
+                $config['system_name'] ?? null,
+                null,
+                '',
+                $config['max_length'] ?? null
             )
         );
 
