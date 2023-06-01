4c973e2c0e155ccbbe3d57f4a70ca298dba40e0a
hedii <contact@hedichaibi.com>
2023-06-01 17:12:53 +0200

    Use LogRecord in test instead of array

--- tests/Fake/AnotherTestProcessor.php
+++ tests/Fake/AnotherTestProcessor.php
@@ -2,9 +2,11 @@
 
 namespace Hedii\LaravelGelfLogger\Tests\Fake;
 
+use Monolog\LogRecord;
+
 class AnotherTestProcessor
 {
-    public function __invoke(array $record): array
+    public function __invoke(LogRecord $record): LogRecord
     {
         return $record;
     }
--- tests/Fake/TestProcessor.php
+++ tests/Fake/TestProcessor.php
@@ -2,9 +2,11 @@
 
 namespace Hedii\LaravelGelfLogger\Tests\Fake;
 
+use Monolog\LogRecord;
+
 class TestProcessor
 {
-    public function __invoke(array $record): array
+    public function __invoke(LogRecord $record): LogRecord
     {
         return $record;
     }
